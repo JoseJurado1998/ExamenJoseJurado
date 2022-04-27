65937d10b406ca3112eef0ab255a636cb79cf7fe
JoseJurado1998 <jose.jurado.repullo@gmail.com>
2022-04-27 09:55:39 +0200

    Commit1

--- prueba1/src/prueba1/suma.java
+++ prueba1/src/prueba1/suma.java
@@ -4,12 +4,11 @@ import javax.swing.JOptionPane;
 
 public class suma {
 
-
-public static void main(String[] args) {
-	int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un número"));
-	int num2 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un número"));
-	
-	System.out.println("La suma es :" +(num1+num2));
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = Integer.parseInt(JOptionPane.showInputDialog("pide num"));
+		int num2 = Integer.parseInt(JOptionPane.showInputDialog("pide num"));
+		System.out.println("suma es :" +(num1+num2));
 	}
+
 }
